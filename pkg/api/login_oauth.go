063edac1dd2f2f072f72802d83dcb7a5bf707433
sonia-elear21 <sonia.dua@healthasyst.com>
2022-12-09 12:48:52 +0530

    revert log

--- pkg/api/login_oauth.go
+++ pkg/api/login_oauth.go
@@ -208,7 +208,6 @@ func (hs *HTTPServer) OAuthLogin(ctx *models.ReqContext) {
 			"refresh_token", fmt.Sprintf("%v", token.RefreshToken),
 		)
 	}
-	fmt.Sprintf("%v", token, "token "),
 
 	// set up oauth2 client
 	client := connect.Client(oauthCtx, token)
